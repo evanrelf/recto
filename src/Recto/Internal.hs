383145296c3b58be74c988a4f651ab73b2c3fb87
Evan Relf <evan@evanrelf.com>
2023-08-22 22:54:04 -0700

    Re-arrange `RecordHasField` type parameters

--- src/Recto/Internal.hs
+++ src/Recto/Internal.hs
@@ -50,19 +50,19 @@ deriving stock instance Show (Record '[])
 deriving stock instance (Show x, Show (Record xs)) => Show (Record (x : xs))
 
 -- | Whether a record has a field.
-class RecordHasField n r a | n r -> a where
+class RecordHasField n a r | n r -> a where
   recordHasField :: Field n -> Record r -> (a -> Record r, a)
 
 instance {-# OVERLAPPING #-} KnownSymbol n
-  => RecordHasField n (n ::: a : r) a where
+  => RecordHasField n a (n ::: a : r) where
   recordHasField n r@(_ `RCons` xs) =
     case recordHasField n r of (_, a) -> (\a' -> n := a' `RCons` xs, a)
 
-instance RecordHasField n r a => RecordHasField n (any : r) a where
+instance RecordHasField n a r => RecordHasField n a (x : r) where
   recordHasField n (x `RCons` xs) =
     case recordHasField n xs of (s, a) -> (\a' -> x `RCons` s a', a)
 
-instance (RecordHasField n r a, KnownSymbol n) => HasField n (Record r) a where
+instance (RecordHasField n a r, KnownSymbol n) => HasField n (Record r) a where
   getField r = case recordHasField (Field (Proxy @n)) r of (_, a) -> a
 
 class RecordFromTuple t r | t -> r, r -> t where
@@ -109,7 +109,7 @@ insert n a r = n := a `RCons` r
 -- example :: Record '["answer" ::: Int] -> Int
 -- example = get #answer
 -- :}
-get :: RecordHasField n r a => Field n -> Record r -> a
+get :: RecordHasField n a r => Field n -> Record r -> a
 get n r = case recordHasField n r of (_, a) -> a
 
 -- | Set field in record.
@@ -120,7 +120,7 @@ get n r = case recordHasField n r of (_, a) -> a
 --   -> Record '["enabled" ::: Bool]
 -- example = set #enabled True
 -- :}
-set :: RecordHasField n r a => Field n -> a -> Record r -> Record r
+set :: RecordHasField n a r => Field n -> a -> Record r -> Record r
 set n a r = case recordHasField n r of (s, _) -> s a
 
 -- | Modify field in record.
@@ -131,5 +131,5 @@ set n a r = case recordHasField n r of (s, _) -> s a
 --   -> Record '["enabled" ::: Bool]
 -- example = modify #enabled not
 -- :}
-modify :: RecordHasField n r a => Field n -> (a -> a) -> Record r -> Record r
+modify :: RecordHasField n a r => Field n -> (a -> a) -> Record r -> Record r
 modify n f r = case recordHasField n r of (s, a) -> s (f a)
